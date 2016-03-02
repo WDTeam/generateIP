c1d7b9f7de60df3bc7f0cad803cf4c249be66cc1
xiayulei <xiayulei@gmail.com>
2016-03-02 09:29:59 +0800

    静态调用

--- IP.class.php
+++ IP.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class IP
  * @Project: generateIP
@@ -6,7 +7,8 @@
  * @Author: xiayulei@gmail.com
  * @Update: 2016.3.2
  */
-class IP {
+class IP
+{
 
     /**
      * 根据省份名（中文）随机生成IPV4地址
@@ -14,37 +16,39 @@ class IP {
      * @param $province
      * @return string
      */
-    public function generateIP($province = ''){
-        $ip_address = $this->_IPSegment($province);
-        $ip = $this->_randomIP($ip_address['begin'],$ip_address['end']);
+    public static function generate($province = '')
+    {
+        $ip_address = self::_IPSegment($province);
+        $ip = self::_randomIP($ip_address['begin'], $ip_address['end']);
         return $ip;
     }
 
     /**
      * 更新各省IPV4地址段
      */
-    public function updateIPSegment(){
+    public static function update()
+    {
         require 'QueryList.class.php';
-        $province = [
-            'BJ','GD','SD','ZJ','JS','SH','LN','SC','HA',
-            'HB','FJ','HN','HE','CQ','SX','JX','SN','AH',
-            'HL','GX','JL','YN','TJ','NM','XJ','GS','GZ',
-            'HI','NX','QH','XZ'
-        ];
+        $province = array(
+            'BJ', 'GD', 'SD', 'ZJ', 'JS', 'SH', 'LN', 'SC', 'HA',
+            'HB', 'FJ', 'HN', 'HE', 'CQ', 'SX', 'JX', 'SN', 'AH',
+            'HL', 'GX', 'JL', 'YN', 'TJ', 'NM', 'XJ', 'GS', 'GZ',
+            'HI', 'NX', 'QH', 'XZ'
+        );
         $count = count($province);
-        $ip_segment = [];
-        for($i=0;$i<$count;$i++){
+        $ip_segment = array();
+        for ($i = 0; $i < $count; $i++) {
             // 采集IP地址段目标网址
-            $url = 'http://ips.chacuo.net/view/s_'.$province[$i];
-            $ip_list = QueryList::Query($url,array('begin'=>array('.v_l','text'),'end'=>array('.v_r','text')),'','UTF-8');
+            $url = 'http://ips.chacuo.net/view/s_' . $province[$i];
+            $ip_list = QueryList::Query($url, array('begin' => array('.v_l', 'text'), 'end' => array('.v_r', 'text')), '', 'UTF-8');
             $ip_array = $ip_list->jsonArr;
             $ip_segment[$province[$i]] = $ip_array;
         }
-        $path = str_replace('\\','/',__DIR__);
-        $file = $path.'/ip_segment.php';
-        $res = $this->_write($file,$ip_segment);
-        if($res){
-        	echo 'Update OK!';
+        $path = str_replace('\\', '/', __DIR__);
+        $file = $path . '/ip_segment.php';
+        $res = self::_write($file, $ip_segment);
+        if ($res) {
+            echo 'Update OK!';
         }
     }
 
@@ -54,19 +58,20 @@ class IP {
      * @param $ip_end
      * @return string
      */
-    private function _randomIP($ip_begin,$ip_end){
-        $ip_begin_array = explode('.',$ip_begin);
-        $ip_end_array = explode('.',$ip_end);
-        $ip1 = mt_rand($ip_begin_array[0],$ip_end_array[0]);
-        $ip2 = mt_rand($ip_begin_array[1],$ip_end_array[1]);
-        $ip3 = mt_rand($ip_begin_array[2],$ip_end_array[2]);
-        $ip4 = mt_rand($ip_begin_array[3],$ip_end_array[3]);
+    private static function _randomIP($ip_begin, $ip_end)
+    {
+        $ip_begin_array = explode('.', $ip_begin);
+        $ip_end_array = explode('.', $ip_end);
+        $ip1 = mt_rand($ip_begin_array[0], $ip_end_array[0]);
+        $ip2 = mt_rand($ip_begin_array[1], $ip_end_array[1]);
+        $ip3 = mt_rand($ip_begin_array[2], $ip_end_array[2]);
+        $ip4 = mt_rand($ip_begin_array[3], $ip_end_array[3]);
 
         // IPV4地址最后一位不为0或255
-        if($ip4 == 0 || $ip4 == 255){
-            $ip4 = mt_rand(2,254);
+        if ($ip4 == 0 || $ip4 == 255) {
+            $ip4 = mt_rand(2, 254);
         }
-        $ip = $ip1.'.'.$ip2.'.'.$ip3.'.'.$ip4;
+        $ip = $ip1 . '.' . $ip2 . '.' . $ip3 . '.' . $ip4;
         return $ip;
     }
 
@@ -76,10 +81,11 @@ class IP {
      * @param $province
      * @return array|string
      */
-    private function _IPSegment($province = ''){
+    private static function _IPSegment($province = '')
+    {
         $ip_segment = [];
         require 'ip_segment.php';
-        $province_array = [
+        $province_array = array(
             '北京' => 'BJ',
             '广东' => 'GD',
             '山东' => 'SD',
@@ -111,15 +117,15 @@ class IP {
             '宁夏' => 'NX',
             '青海' => 'QH',
             '西藏' => 'XZ'
-        ];
-        if(empty($province)){
-            $ip_segment_array = $ip_segment[$province_array[array_rand($province_array,1)]];
-        }elseif(array_key_exists($province,$province_array)){
+        );
+        if (empty($province)) {
+            $ip_segment_array = $ip_segment[$province_array[array_rand($province_array, 1)]];
+        } elseif (array_key_exists($province, $province_array)) {
             $ip_segment_array = $ip_segment[$province_array[$province]];
-        }else{
+        } else {
             return false;
         }
-        return $ip_segment = $ip_segment_array[array_rand($ip_segment_array,1)];
+        return $ip_segment = $ip_segment_array[array_rand($ip_segment_array, 1)];
     }
 
     /**
@@ -130,14 +136,15 @@ class IP {
      * @param bool|false $format
      * @return bool
      */
-    private function _write($filename,$values,$var='ip_segment',$format=false){
-        $file=$filename;
-        if(is_array($values)){
-            $text="<?php\r\n".'$'.$var.'='.$this->_arrayConvert($values,$format).";";
-        }else{
-            $text=$values;
+    private static function _write($filename, $values, $var = 'ip_segment', $format = false)
+    {
+        $file = $filename;
+        if (is_array($values)) {
+            $text = "<?php\r\n" . '$' . $var . '=' . self::_arrayConvert($values, $format) . ";";
+        } else {
+            $text = $values;
         }
-        return $this->_writeFile($file,$text);
+        return self::_writeFile($file, $text);
     }
 
     /**
@@ -147,28 +154,29 @@ class IP {
      * @param int $level
      * @return string
      */
-    private function _arrayConvert($array,$format=false,$level=0){
-        $space=$line='';
-        if(!$format){
-            for($i=0;$i<=$level;$i++){
-                $space.="\t";
+    private static function _arrayConvert($array, $format = false, $level = 0)
+    {
+        $space = $line = '';
+        if (!$format) {
+            for ($i = 0; $i <= $level; $i++) {
+                $space .= "\t";
             }
-            $line="\n";
+            $line = "\n";
         }
-        $evaluate='Array'.$line.$space.'('.$line;
-        $comma=$space;
-        foreach($array as $key=> $val){
-            $key=is_string($key)?'\''.addcslashes($key,'\'\\').'\'':$key;
-            $val=!is_array($val)&&(!preg_match('/^\-?\d+$/',$val)||strlen($val) > 12)?'\''.addcslashes($val,'\'\\').'\'':$val;
-            if(is_array($val)){
-                $evaluate.=$comma.$key.'=>'.$this->_arrayConvert($val,$format,$level+1);
-            }else{
-                $evaluate.=$comma.$key.'=>'.$val;
+        $data = 'Array' . $line . $space . '(' . $line;
+        $comma = $space;
+        foreach ($array as $key => $val) {
+            $key = is_string($key) ? '\'' . addcslashes($key, '\'\\') . '\'' : $key;
+            $val = !is_array($val) && (!preg_match('/^\-?\d+$/', $val) || strlen($val) > 12) ? '\'' . addcslashes($val, '\'\\') . '\'' : $val;
+            if (is_array($val)) {
+                $data .= $comma . $key . '=>' . self::_arrayConvert($val, $format, $level + 1);
+            } else {
+                $data .= $comma . $key . '=>' . $val;
             }
-            $comma=','.$line.$space;
+            $comma = ',' . $line . $space;
         }
-        $evaluate.=$line.$space.')';
-        return $evaluate;
+        $data .= $line . $space . ')';
+        return $data;
     }
 
     /**
@@ -178,13 +186,14 @@ class IP {
      * @param string $openMode
      * @return bool
      */
-    private function _writeFile($filename,$text,$openMode='w'){
-        if(false!==$fp=fopen($filename,$openMode)){
-            flock($fp,2);
-            fwrite($fp,$text);
+    private static function _writeFile($filename, $text, $openMode = 'w')
+    {
+        if (false !== $fp = fopen($filename, $openMode)) {
+            flock($fp, 2);
+            fwrite($fp, $text);
             fclose($fp);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
